5d3f82efa6413da71d62a0ddd169a6edba1283a1
sandbava <bsandoz.pro@gmail.com>
2024-11-01 14:03:08 +0100

    Deserialise request content into webhook

--- src/Controller/WebhooksController.php
+++ src/Controller/WebhooksController.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\Webhook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class WebhooksController extends AbstractController
 {
-    #[Route(path: '/webhook', name: 'webhook', methods: ['POST'])]
-    public function __invoke(): Response
+    public function __construct(
+        private SerializerInterface $serializer,
+    )
     {
-        return new Response(status: 204);
+    }
+
+    #[Route(path: '/webhook', name: 'webhook', methods: ['POST'])]
+    public function __invoke(Request $request): Response
+    {
+        try {
+            $webhook = $this->serializer->deserialize($request->getContent(), Webhook::class, 'json');
+            $webhook->setRawPayload($request->getContent());
+
+            return new Response(status: 204);
+        } catch (\Throwable $exception) {
+            throw $exception;
+        }
+
+
     }
 }
