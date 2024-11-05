15ddb5fc4f30f092193230313b65028e2beef379
sandbava <bsandoz.pro@gmail.com>
2024-11-05 16:28:59 +0100

    Create SubscriptionStartForwarder

---
+++ src/Forwarder/Newsletter/Identify/SubscriptionStartForwarder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Forwarder\Newsletter\Identify;
+
+use App\DTO\Newsletter\NewsletterWebhook;
+use App\Forwarder\Newsletter\ForwarderInterface;
+
+class SubscriptionStartForwarder implements ForwarderInterface
+{
+    private const SUPPORTED_EVENT = 'newsletter_subscribed';
+
+    public function supports(NewsletterWebhook $newsletterWebhook): bool
+    {
+        return $newsletterWebhook->getEvent() === self::SUPPORTED_EVENT;
+    }
+
+    public function forward(NewsletterWebhook $newsletterWebhook): void
+    {
+        // TODO: Implement forward() method.
+    }
+}
