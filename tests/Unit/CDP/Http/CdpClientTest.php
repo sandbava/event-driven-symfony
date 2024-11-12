b634001fc63512b546f3490c985e5e93ccce6a61
sandbava <bsandoz.pro@gmail.com>
2024-11-12 11:20:40 +0100

    Fix CdpClientTest

--- tests/Unit/CDP/Http/CdpClientTest.php
+++ tests/Unit/CDP/Http/CdpClientTest.php
@@ -21,7 +21,7 @@ class CdpClientTest extends TestCase
 
         $mockHttpClient = new MockHttpClient($responses);
 
-        $unit = new CdpClient($mockHttpClient, 'fake-api-key');
+        $unit = new CdpClient($mockHttpClient, 'fake-api-key', 'http://some-fake-url');
 
         try {
             $mockTrackModel = $this->createMock(ModelInterface::class);
